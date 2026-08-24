c871c693cb3523c7ae0952f420142c9e5cd4482c
HIMMY20 <chauhanmhimanshu2005@gmail.com>
2026-08-24 23:16:55 +0530

    New Database

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-# Render is script - build ke time automatically chalti hai
 set -o errexit
 
 pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
-python manage.py migrate
+python manage.py migrate --noinput
